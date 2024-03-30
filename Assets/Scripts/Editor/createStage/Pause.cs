7f3dad345ec2b7313bc71097729cdc7034f9e08c
DESKTOP-8OIJJMV\himak <g2027050@st.t-kougei.ac.jp>
2024-03-31 02:07:45 +0900

    commit

--- Assets/Scripts/Editor/createStage/Pause.cs
+++ Assets/Scripts/Editor/createStage/Pause.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
-	[SerializeField]
-	//　ポーズした時に表示するUIのプレハブ
-	private GameObject pauseUIPrefab;
-	//　ポーズUIのインスタンス
-	private GameObject pauseUIInstance;
+    [SerializeField]
+    //　ポーズした時に表示するUIのプレハブ
+    private GameObject pauseUIPrefab;
+    //　ポーズUIのインスタンス
+    private GameObject pauseUIInstance;
 
-	public bool PauseFlag = true;
+    public bool PauseFlag = true;
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKeyDown("q"))
-		{
-			if (pauseUIInstance == null)
-			{
-				pauseUIInstance = GameObject.Instantiate(pauseUIPrefab) as GameObject;
-				Time.timeScale = 0f;
-				PauseFlag = false;
-			}
-			else
-			{
-				Destroy(pauseUIInstance);
-				Time.timeScale = 1f;
-				PauseFlag = true;
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            if (pauseUIInstance == null)
+            {
+                pauseUIInstance = GameObject.Instantiate(pauseUIPrefab) as GameObject;
+                Time.timeScale = 0f;
+                PauseFlag = false;
+            }
+            else
+            {
+                Destroy(pauseUIInstance);
+                Time.timeScale = 1f;
+                PauseFlag = true;
+            }
+        }
+    }
 }
